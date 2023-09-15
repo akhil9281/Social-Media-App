8c0282036ad8eaa4e66ed76cfac77fb0571c89fd
Akhil Kumar <88153463+akhil9281@users.noreply.github.com>
2023-09-15 13:53:02 +0530

    trimming extra

--- frontEndFeed/script/controller/feedController.js
+++ frontEndFeed/script/controller/feedController.js
@@ -43,33 +43,6 @@ angular.module("feedApp")
         
     }
 
-/*     $scope.getMostLikedPost = function() {
-        $scope.mostLikedPost = {};
-        feedService.getMostLikedPost()
-        .then(function(value) {
-            console.log("mostLikedPost - ", value.data[0]);
-            $scope.mostLikedPost = value.data[0];            
-        })
-    };
-
-    $scope.getMostDiscussedQuestion = function() {
-        $scope.mostDiscussedQuestion = {};
-        feedService.getMostDiscussedQuestion()
-        .then(function(value) {
-            console.log("mostDiscussedPost - ", value.data[0]);
-            $scope.mostDiscussedQuestion = value.data[0];            
-        })
-    }; 
-
-    $scope.getQuote = function() {
-        $scope.quote = {};
-        feedService.getQuote()
-            .then(function(value) {
-                console.log("Quote is - ", value);
-                $scope.quote = value[0];
-            })
-    };
-    */
 
     $scope.loadMorePosts = function() {
         if (!$scope.isLoading) {
